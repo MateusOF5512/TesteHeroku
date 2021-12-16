7256e591b5d7fb14de976b37b739288e11ce2fc2
M-Ortiz <69127852+MateusOF5512@users.noreply.github.com>
2021-12-16 17:10:33 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
-mkdir -p ~/.streamlit/
-
+echo "\
+[general]\n\
+email = \"mateus210m@hotmail.com\"\n\
+" > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
-port = $PORT\n\
-enableCORS = false\n\
 headless = true\n\
-\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
